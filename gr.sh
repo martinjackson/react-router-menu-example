8294b96b5caf99354330ea75bf675292990a69e6
Martin Jackson <martin.a.jackson@gmail.com>
2018-07-18 21:50:20 -0500

    graphing code dependancies with npm package madge

---
+++ gr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# sudo npm -g install madge
+
+madge src --image image.svg
+madge src --no-color --image image-bw.svg
+
+
---
+++ scan.js
@@ -0,0 +1,10 @@
+const madge = require('madge');
+
+madge('src/index.js').then((res) => {
+	console.log(res.obj());
+        console.log('-------------------------------------------');
+	console.log(res);
+        console.log('-------------------------------------------');
+	console.log(res.orphans());
+});
+
